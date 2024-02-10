90ffa7b842bf0ddd49c7fe8b11d03f42e709fd75
unknown <karloseq@gmail.com>
2024-02-10 00:59:49 +0000

    week03

---
+++ week03/testTypes.py
@@ -0,0 +1,13 @@
+# Define variables of different types
+i = 3
+fl = 3.5
+isa = True
+memo = 'how now Brown Cow'
+lots = []
+
+# Print the variables along with their types
+print('variable {} is of type:{} and value:{}'.format('i', type(i), i))
+print('variable {} is of type:{} and value:{}'.format('fl', type(fl), fl))
+print('variable {} is of type:{} and value:{}'.format('is', type(isa), isa))
+print('variable {} is of type:{} and value:{}'.format('memo', type(memo), memo))
+print('variable {} is of type:{} and value:{}'.format('lots', type(lots), lots))
