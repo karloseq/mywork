ebf56be791550c0181fdab13cf235ce43b4ccdb3
Karol Makarewicz <karloseq@gmail.com>
2024-02-22 23:49:39 +0000

    Week04 practice

---
+++ week04/testBoolean.py
@@ -0,0 +1,12 @@
+
+is_alive = True
+
+print (f"is alive {is_alive}")
+
+var = (2 == 4)
+print (f"var is {var}")
+
+grade = 40
+invalid = (grade< 0) or (grade >100)
+print (f"invalid is {invalid}")
+
---
+++ week04/testif.py
@@ -0,0 +1,21 @@
+b = 1
+if True:
+    print("we are in the if statement")
+    b = 22
+    print("sanity", b)
+
+    c = 1
+    if c == 1:
+        print("c is one")
+else:
+    print("c is not one")
+
+d = 4
+if d < 0:
+    print("d is negative")
+elif d > 10:
+    print("d is 10 or higher")
+else:
+    print("d is between 0 and 9 (inclusive)")
+
+print("sanity", b)  # This print statement is outside of any block
